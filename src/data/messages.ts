2db468aa6e3cec41cad279d33ad28caa3a680065
exhan <69556083+exeptionhandler@users.noreply.github.com>
2024-12-12 21:16:38 -0500

    Add files via upload

---
+++ src/data/messages.ts
@@ -0,0 +1,7 @@
+export const suggestedMessages = [
+  'Wishing you a magical and blissful holiday season!',
+  'May the spirit of Christmas bring you peace and happiness!',
+  'Sending warm wishes and holiday cheer your way!',
+  'Hope your holidays sparkle with joy and laughter!',
+  'May your Christmas be filled with wonder and delight!'
+];
